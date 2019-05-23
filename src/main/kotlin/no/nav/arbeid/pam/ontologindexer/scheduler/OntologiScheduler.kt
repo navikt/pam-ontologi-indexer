a82dc0d20041f774bdddfb8efb7fe27cf1a8b168
joar <joar.aurdal@gmail.com>
2019-05-23 14:15:53 +0200

    fixed delay

--- src/main/kotlin/no/nav/arbeid/pam/ontologindexer/scheduler/OntologiScheduler.kt
+++ src/main/kotlin/no/nav/arbeid/pam/ontologindexer/scheduler/OntologiScheduler.kt
@@ -15,7 +15,8 @@ class OntologiScheduler {
     @Autowired
     lateinit var jobbtittelIndexerService: JobbtittelIndexerService
 
-    @Scheduled(cron = CRON_INDEXER)
+    //@Scheduled(cron = CRON_INDEXER)
+    @Scheduled(fixedDelay = 120000)
     fun leggInnStillingstitler() {
         LOGGER.info("leggInnStillingstitler scheduled job startet cron:$CRON_INDEXER")
         jobbtittelIndexerService.indekser()
